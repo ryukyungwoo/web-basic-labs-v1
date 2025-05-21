3cf03043bb439436e6c99d4b77eb1cde16d6350a
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-05-21 10:31:58 +0900

    http/css 1일차

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* 📜 tailwind.config.ts */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./components/**/*{.jsx,.tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
